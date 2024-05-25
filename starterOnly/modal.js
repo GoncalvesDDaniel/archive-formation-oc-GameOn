a73d98fb5015d3d3e7ed6c670b56ac2ccf2d5747
Daniel D GONCALVES <107401263+GoncalvesDDaniel@users.noreply.github.com>
2024-05-26 00:48:57 +0200

    Add close btn for form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,31 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
+// Close modal event
+modalBtnClose.addEventListener("click", () => {
+    modalbg.style.display = "none";
+});
 
+// TODO : mettre event.preventDefault pour le bouton submit
+// ? Est-ce qu'on peut faire un .reduce pour trouver le input[type=radio] ou for obligé?
+// ? pourquoi 2 boutons je m'inscris
