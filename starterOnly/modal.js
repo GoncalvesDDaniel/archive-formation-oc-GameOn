13e02e77915d295cf50a719101b9759867585c96
Daniel D GONCALVES <107401263+GoncalvesDDaniel@users.noreply.github.com>
2024-05-27 19:42:37 +0200

    Fix first and last name checker with error

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,8 +14,8 @@ const formdata = document.querySelectorAll(".formdata");
 const modalbtnclose = document.querySelector(".close");
 const submitBtn = document.querySelector(".btn-submit");
 const modalform = document.querySelector("form");
-const modalPrenomField = document.querySelector("#first");
-const modalNomField = document.querySelector("#last");
+const modalPrenomInput = document.querySelector("#first");
+const modalNomInput = document.querySelector("#last");
 
 //* launch modal event
 modalbtn.forEach((btn) => btn.addEventListener("click", launchmodal));
@@ -36,34 +36,33 @@ modalform.addEventListener("submit", (event) => {
 });
 
 //* first / last name regex checker
-const isNameInputValid = (str) => {
+const isNameInputValid = (input, inputName) => {
     const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s'-]{2,}$/;
-
-    if (str.value.trim().length < 2) {
-        console.log(" votre réponse n'est pas assez long");
+    let result;
+    if (input.value.trim().length < 2) {
+        input.parentNode.dataset.errorVisible = true;
+        input.parentNode.dataset.error = `*Votre ${inputName} n'est pas assez long.`;
         return;
     }
-    if (regex.test(str.value) === false) {
-        console.log(str.value + " n'est pas valide.");
+    if (regex.test(input.value) === false) {
+        input.parentNode.dataset.errorVisible = true;
+        input.parentNode.dataset.error = `*Votre ${inputName} comporte un caratère invalide.`;
         return;
     } else {
-        console.log(str.value + " prenom valid");
+        input.parentNode.dataset.errorVisible = false;
+        input.parentNode.removeAttribute("data-error-visible");
+        input.parentNode.removeAttribute("data-error");
         return;
     }
 };
-
 //* prenom validation
-modalPrenomField.addEventListener("change", () => {
-    // const userPrenom = event.target.value;
-    isNameInputValid(modalPrenomField);
-    console.log("prenom f() fini");
+modalPrenomInput.addEventListener("change", () => {
+    isNameInputValid(modalPrenomInput, "prénom");
 });
 
 //* nom validation
-modalNomField.addEventListener("change", () => {
-    // const userPrenom = event.target.value;
-    isNameInputValid(modalNomField);
-    console.log("nom f() fini");
+modalNomInput.addEventListener("change", () => {
+    isNameInputValid(modalNomInput, "nom");
 });
 
 //* modal validation
@@ -72,5 +71,11 @@ function validate() {
 }
 
 // TODO : mettre event.preventDefault pour le bouton submit
+
 // ? Est-ce qu'on peut faire un .reduce pour trouver le input[type=radio] ou for obligé?
 // ? pourquoi 2 boutons je m'inscris
+
+//! Set attribut to formData for the error - font-size: 10px
+// formData[data-error]::after ou
+// .formData[data-error-visible="true"]::after
+// email.parentNode.dataset.errorVisible = 'true'; permet de sélectionner le partent de formData et de mettre l'attribut data-errorVisible
